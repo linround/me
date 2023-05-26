dc9c285d712e4a417bdcefe3975c204d94f72d06
linyuan <673609957@qq.com>
2023-05-26 16:57:19 +0800

    stage: 2023-05-26

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Link from "next/link";
+import style from './page.module.css'
 
 
 export default function Home() {
   return (
-    <div>
-      <p>In 2020, I got my BEng degree in Electronic and Information Engineering form Communication University of China.</p>
-    </div>
+    <ul className={style.container}>
+      <li>In 2020, I got my BEng degree in Electronic and Information Engineering form <Link
+          className={style.link}
+          target={'_blank'}
+          href={'https://www.cuc.edu.cn/'}>
+          Communication University of China
+        </Link>
+        .</li>
+      <li>In 2020, Start learning front-end.</li>
+      <li></li>
+    </ul>
+
   )
 }
