57a4c692ec951783fe2f860beb4bc7bc0a85c729
linyuan <673609957@qq.com>
2023-09-08 19:16:59 +0800

    doc:name

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import css from "./page.module.css";
 export default function Home() {
   return (
     <div className={css.container}>
+
       <iframe width="640" height="360" frameBorder="0"
               src="https://www.shadertoy.com/embed/mtByWc?gui=true&t=10&paused=true&muted=false"
               allowFullScreen></iframe>
@@ -13,6 +14,15 @@ export default function Home() {
               allowFullScreen></iframe>
       <iframe width="640" height="360" frameBorder="0"
               src="https://www.shadertoy.com/embed/DlSyDW?gui=true&t=10&paused=true&muted=false"
+              allowFullScreen></iframe>
+      <iframe width="640" height="360" frameBorder="0"
+              src="https://www.shadertoy.com/embed/mlfBWB?gui=true&t=10&paused=true&muted=false"
+              allowFullScreen></iframe>
+      <iframe width="640" height="360" frameBorder="0"
+              src="https://www.shadertoy.com/embed/DtsfRB?gui=true&t=10&paused=true&muted=false"
+              allowFullScreen></iframe>
+      <iframe width="640" height="360" frameBorder="0"
+              src="https://www.shadertoy.com/embed/DlSBzz?gui=true&t=10&paused=true&muted=false"
               allowFullScreen></iframe>
     </div>
 
