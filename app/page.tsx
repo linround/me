ed16e093ab14f91394ed210f7c6e35af60f693ef
linyuan1105 <673609957@qq.com>
2023-09-23 12:41:40 +0800

    feat: ray tracing

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,9 @@ import css from "./page.module.css";
 export default function Home() {
   return (
     <div className={css.container}>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/ds3yzf?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
+        <iframe width="640" height="360" frameBorder="0"
+                src="https://www.shadertoy.com/embed/dddyDB?gui=true&t=10&paused=true&muted=false"
+                allowFullScreen></iframe>
       <iframe width="640" height="360" frameBorder="0"
               src="https://www.shadertoy.com/embed/mtByWc?gui=true&t=10&paused=true&muted=false"
               allowFullScreen></iframe>
