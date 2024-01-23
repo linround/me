43d9386d221fced458c7872f8161a19cd3234bca
linyuan <yuanlincuc@gmail.com>
2024-01-23 14:04:42 +0800

    refactor: graphicsLibraryPage

--- ui/layoutFooter.tsx
+++ ui/layoutFooter.tsx
@@ -5,7 +5,6 @@ import style from "./layoutFooter.module.css";
 export function LayoutFooter() {
   return (
     <footer className={style.container}>
-      <div>
         联系与帮助：
         <Link
           className={style.contact}
@@ -18,17 +17,6 @@ export function LayoutFooter() {
           href={'mailto:yuanlincuc@gmail.com'}>
           Gmail
         </Link>
-      </div>
-      <div>
-        版权所有：
-        <Link
-          className={style.contact}
-          target={'_blank'}
-          href={'https://beian.miit.gov.cn/#/Integrated/index'}>
-
-          湘ICP备2023007031号-1
-        </Link>
-      </div>
     </footer>
   )
 }
