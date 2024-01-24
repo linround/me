26b221142b0cff72d74ca46319243eae530f6087
linyuan <yuanlincuc@gmail.com>
2024-01-24 12:51:33 +0800

    refactor: component profile

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,4 @@
 import style from "./page.module.css";
-import Link from "next/link";
 import {ArticleLink} from "@/ui/articleLink";
 
 export const metadata = {
@@ -13,6 +12,7 @@ export default function About() {
         </li>
         <li className={style.listItem}>I am a front-end developer.</li>
 
+        <img className={style.profile} src={'/profile.svg'}/>
         <li className={style.caption}>
           看了一些非技术类的书籍，例如一些心理学，社会学，人物传记。此时，我对因果深信不移。我也打算在此种下因的种子，希望可以由此给你带来一些帮助。
         </li>
