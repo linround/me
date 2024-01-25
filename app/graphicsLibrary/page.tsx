fc1ccca433668ddacd1d14edb0ad0561d26a4890
linyuan <yuanlincuc@gmail.com>
2024-01-25 12:15:34 +0800

    refactor: font setting

--- app/graphicsLibrary/page.tsx
+++ app/graphicsLibrary/page.tsx
@@ -12,29 +12,61 @@ const graphicsLibraryLinks = [
   {
     url: 'https://github.com/ecomfe/zrender',
     name:'zrender'
+  },
+  {
+    url: 'https://github.com/konvajs/konva',
+    name:'konva'
   }
 ]
 export default function Page() {
   return (
     <div>
       <h1 className={style.title}>{graphicsLibraryRouter.description}</h1>
-      <p className={style.caption}>
-        人的幸福感，最终只能源自内心的安宁。
-      </p>
-      <div className={style.content}>
-        {
-          graphicsLibraryLinks.map(link=>{
-            return (
-              <div key={link.url} className={style.linkUrl}>
-                <Link
-                  className={style.linkText}
-                  href={link.url}
-                  target={'_blank'}>{link.url}</Link>
-              </div>
-            )
-          })
-        }
-      </div>
+      <article>
+        <p className={style.caption}>
+          人的幸福感，最终只能源自内心的安宁。
+        </p>
+        <p className={style.content}>
+          {
+            graphicsLibraryLinks.map(link=>{
+              return (
+                <p key={link.url} className={style.linkUrl}>
+                  <Link
+                    className={style.linkText}
+                    href={link.url}
+                    target={'_blank'}>{link.url}</Link>
+                </p>
+              )
+            })
+          }
+        </p>
+        <footer className={style.footer}>
+          以上是基于
+          <strong>
+           canvas
+          </strong>
+          实现的
+          <strong>
+            2D
+          </strong>
+          图形库，在很多数据可视化的项目中起到很大的作用。
+          <br/>
+          <strong>
+            <Link className={style.linkText} href={'https://github.com/apache/echarts'} target={'_blank'}>echart</Link>
+          </strong>是基于
+          <strong>
+            <Link className={style.linkText} href={'https://github.com/ecomfe/zrender'} target={'_blank'}>zrender</Link>
+          </strong>；<br/>
+          <strong>
+            <Link className={style.linkText} href={'https://github.com/VisActor/VChart'} target={'_blank'}>vchart</Link>
+          </strong>
+          部分源码参考了
+          <strong>
+            <Link className={style.linkText} href={'https://github.com/konvajs/konva'} target={'_blank'}>konva</Link>
+          </strong>。
+        </footer>
+      </article>
+
 
     </div>
   )
