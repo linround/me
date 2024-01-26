d2f79429c03ef0969d09b8f8390bfc93633956ae
linyuan <yuanlincuc@gmail.com>
2024-01-26 16:47:11 +0800

    refactor: photomosh

---
+++ app/photomosh/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+export default function Page() {
+  return (
+    <article>
+      <Link href={'https://photomosh.com/pro/'}>photomosh</Link>
+      <p>
+        这是一个很有意思的，实时视觉效果混合器，可对图片、视频 和 gif 进行创意控制。
+      </p>
+
+    </article>
+  )
+}
