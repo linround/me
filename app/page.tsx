b812bd32c1f21e1952fa99efc2a2fce114ecdf34
linyuan <yuanlincuc@gmail.com>
2024-01-26 12:23:06 +0800

    refactor: loading iframe

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,41 @@
 'use client'
 import css from "./page.module.css";
 import {useEffect} from "react";
+import {IframeComponent} from "@/ui/Iframe";
 
 
 
+const Shaders = [
+  {
+    url:'https://www.shadertoy.com/embed/dddyDB?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/mtByWc?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/dtfyRl?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/DlSyDW?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/mlfBWB?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/DtsfRB?gui=true&t=10&paused=true&muted=false'
+  },
+  {
+    url:'https://www.shadertoy.com/embed/DlSBzz?gui=true&t=10&paused=true&muted=false'
+  },
+]
 export default function Home() {
   useEffect(()=>{
   })
   return (
     <div className={css.container}>
-        <iframe width="640" height="360" frameBorder="0"
-                src="https://www.shadertoy.com/embed/dddyDB?gui=true&t=10&paused=true&muted=false"
-                allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/mtByWc?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/dtfyRl?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/DlSyDW?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/mlfBWB?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/DtsfRB?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
-      <iframe width="640" height="360" frameBorder="0"
-              src="https://www.shadertoy.com/embed/DlSBzz?gui=true&t=10&paused=true&muted=false"
-              allowFullScreen></iframe>
+      {Shaders.map(shader=>{
+        return (<IframeComponent src={shader.url} key={shader.url}/>)
+      })}
     </div>
 
   )
---
+++ ui/Iframe.tsx
@@ -0,0 +1,31 @@
+'use client'
+import {createRef, useEffect, useState} from "react";
+import style from './iframe.module.css'
+import clsx from "clsx";
+
+interface IProps {
+  src:string
+}
+export function IframeComponent(props:IProps) {
+  const {src} = props
+  const ref = createRef<HTMLIFrameElement>()
+  const [loading,setLoading] = useState<boolean>(true)
+  useEffect(()=>{
+    if(ref.current){
+      const iframe = ref.current
+      iframe.onload = function () {
+        setLoading(false)
+      }
+    }
+  },[ref])
+  return (
+    <iframe
+      className={clsx([loading && style.iframe])}
+      ref={ref}
+      width="640"
+      height="360"
+      frameBorder="0"
+      src={src}
+      allowFullScreen/>
+  )
+}
